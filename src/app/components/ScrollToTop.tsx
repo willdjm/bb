c97ad39b92c306d035ab2291815425a43cdbc0a5
Wilkinson <wilkinsondjm@gmail.com>
2024-07-18 22:48:47 -0300

    first commit

--- src/app/components/ScrollToTop.tsx
+++ src/app/components/ScrollToTop.tsx
@@ -1,16 +1,43 @@
-import Link from "next/link";
-import { GoMoveToTop } from "react-icons/go";
+import React, { useState, useEffect } from 'react';
+import { GoMoveToTop } from 'react-icons/go';
+import Link from 'next/link';
 
 export function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
 
-return (
+  useEffect(() => {
+    // Função para verificar a posição de rolagem da página
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      // Exibir o botão quando a posição de rolagem for maior que 400 pixels
+      if (currentScrollY > 400) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
 
-<div className="fixed right-5 bottom-3 md:group z-50" >
-    <Link
-        className="flex items-center justify-center text-blue-600 bg-slate-100 rounded-lg w-14 h-14 hover:bg-slate-200 cursor-pointer"
-        href='/'>
-        <GoMoveToTop size={30} />
-    </Link>
-</div>
-);
+    // Adiciona um event listener para o evento de rolagem
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove o event listener ao desmontar o componente para evitar vazamentos de memória
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // O array vazio assegura que o useEffect só será executado uma vez, sem dependências
+
+  // Renderiza o botão somente se isVisible for true
+  return (
+    <>
+      {isVisible && (
+        <div className="fixed right-5 bottom-3 md:group z-50">
+          <Link href="/">
+            <a className="flex items-center justify-center text-blue-600 bg-slate-100 rounded-lg w-14 h-14 hover:bg-slate-200 cursor-pointer">
+              <GoMoveToTop size={30} />
+            </a>
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
