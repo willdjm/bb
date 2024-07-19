717136ba03ff304d62f37b275c84aa4e4d3fc8f5
Wilkinson <wilkinsondjm@gmail.com>
2024-07-18 22:45:16 -0300

    first commit

--- src/app/components/ScrollToTop.tsx
+++ src/app/components/ScrollToTop.tsx
@@ -1,5 +1,5 @@
+import Link from "next/link";
 import { GoMoveToTop } from "react-icons/go";
-import { Link } from "react-scroll";
 
 export function ScrollToTop() {
 
@@ -8,8 +8,7 @@ return (
 <div className="fixed right-5 bottom-3 md:group z-50" >
     <Link
         className="flex items-center justify-center text-blue-600 bg-slate-100 rounded-lg w-14 h-14 hover:bg-slate-200 cursor-pointer"
-        to="Top"
-        spy={true} smooth={true} offset={-100} duration={500} delay={100}>
+        href='/'>
         <GoMoveToTop size={30} />
     </Link>
 </div>
